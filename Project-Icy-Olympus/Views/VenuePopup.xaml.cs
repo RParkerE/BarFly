8881f2950e574af3e4f58add54789cb64597aa87
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2023-07-27 14:36:41 -0500

    Add voting popup

--- Project-Icy-Olympus/Views/VenuePopup.xaml.cs
+++ Project-Icy-Olympus/Views/VenuePopup.xaml.cs
@@ -1,5 +1,7 @@
 using Mapsui;
+using Mopups.Interfaces;
 using Mopups.Pages;
+using Mopups.Services;
 using Project_Icy_Olympus.Models;
 using System.Diagnostics;
 
@@ -8,12 +10,16 @@ namespace Project_Icy_Olympus.Views;
 public partial class VenuePopup : PopupPage
 {
 	IFeature? pInfo;
-	public VenuePopup(IFeature? pInfo)
+    public VenuePopup(IFeature? pInfo)
 	{
 		InitializeComponent();
         this.pInfo = pInfo;
 		string info = this.pInfo["name"].ToString() + "\n" + this.pInfo["address"].ToString() + "\n" + this.pInfo["ratings"].ToString() + "\n" + this.pInfo["price"].ToString() + "\n" + this.pInfo["vibes"].ToString();
         Info.Text = info;
-		Debug.WriteLine(this.pInfo);
+    }
+
+    async void OnButtonClicked(object sender, EventArgs args)
+    {
+        await MopupService.Instance.PushAsync(new VibeVotePopup());
     }
 }
---
+++ Project-Icy-Olympus/Views/VibeVotePopup.xaml.cs
@@ -0,0 +1,11 @@
+using Mopups.Pages;
+
+namespace Project_Icy_Olympus.Views;
+
+public partial class VibeVotePopup : PopupPage
+{
+	public VibeVotePopup()
+	{
+		InitializeComponent();
+	}
+}
